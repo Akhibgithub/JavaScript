4f21e20200ba190aa7cfd7fe0fddc40f9e9f518f
Akhibgithub <stark010402@gmail.com>
2023-01-02 14:33:04 +0530

    Random text generator project done.

--- projects/Lorem Ipsum/app.js
+++ projects/Lorem Ipsum/app.js
@@ -19,5 +19,22 @@ const text = [
   form.addEventListener('submit', (e) =>{
     e.preventDefault()
    const value =parseInt(amount.value) ;
-   console.log(value);
+   const random = Math.floor(Math.random() * text.length)
+//    console.log(value);
+
+   //empty
+   // -1
+   // > 9
+   if(isNaN(value) || value <= 0 || value > 9){
+    result.innerHTML = `<p class="result">${text[random]}</p>`
+   } else{
+    let tempText = text.slice(0,value)
+    // console.log(tempText);
+    tempText = tempText.map((item) =>{
+        return `<p class="result">${item}</p>`
+    })
+    .join('')
+    result.innerHTML = tempText
+    // console.log(tempText);
+   }
 })
